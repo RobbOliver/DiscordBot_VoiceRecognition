432213e8f467bc82bf1cf237b9d76b85a82a42f0
RobbOliver <robsoncaetano@email.com>
2023-01-11 15:25:33 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const tokenConfig = require("./config.json");
 
+const tokenConfig = require("./config.json");
 const client = new Client({intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
 ],});
-
-
 const TOKEN = tokenConfig.token;
 
 
-client.on('ready', (c) => {
-    console.log(`${client.user.username} ready!`)
+client.on('ready', (c) => {                             // When BOT is Started;
+    client.user.setStatus("dnd");                       // Setting Status as Do Not Disturb
+    client.user.setActivity("Under Development");       // Setting Activity
 });
 
-client.on('messageCreate', (msg) => {
-    if(msg.content === 'Ping') { msg.reply('Pong') }
-});
+
+
 
 client.login(TOKEN)
 .then(() => console.log(`${client.user.username} logged!`))
