d0bffed392125c478911144d2c117b9a3af87e8e
RobbOliver <robsoncaetano@email.com>
2023-01-18 13:28:31 -0300

    micro alt

--- src/commands/AStartSTT.ts
+++ src/commands/AStartSTT.ts
@@ -7,7 +7,7 @@ module.exports = {
         .setName('stt')
         .setDescription('Start a Speech-To-Text'),
     async execute(interaction,client) {
-        interaction.reply({ content: 'test' })
+        interaction.reply({ content: 'STT started' })
         
         const voiceConnection = joinVoiceChannel({
             channelId: "998686330324275273",
